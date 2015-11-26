f2c4977eec2e15ae424c71d42fa0927e5dd82ea0
vr <vr@incterlogic.com.ua>
2015-11-26 10:18:51 +0200

    updated lang

--- Shop/Filters/LocalizationFilter.cs
+++ Shop/Filters/LocalizationFilter.cs
@@ -11,13 +11,18 @@ namespace Shop.Filters
 {
     public class LocalizationFilter : ActionFilterAttribute
     {
-        public override void OnResultExecuting(ResultExecutingContext context)
+		public override void OnActionExecuting(ActionExecutingContext context)
         {
-            base.OnResultExecuting(context);
             Language lang = AppSettings.DeafaultLanguage;
+			var param = context.HttpContext.Request.QueryString["lang"];
+			if (param != null)
+			{
+				Enum.TryParse(param, true, out lang);
+				context.HttpContext.Session["lang"] = lang;
+			}
             if (context.HttpContext.Session != null && context.HttpContext.Session["lang"] != null)
             {
-                Enum.TryParse(context.HttpContext.Session["lang"].ToString(), out lang);
+	            lang = (Language) (context.HttpContext.Session["lang"]);
             }
 
 	        context.HttpContext.Items["Translations"] =
